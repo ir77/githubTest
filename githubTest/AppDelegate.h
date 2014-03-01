e04378395a40fdfb343485f7438277743464c04c
ir77 <fly.oh.shogun.iridium.77@gmail.com>
2014-03-01 15:55:41 +0900

    Initial Commit

---
+++ githubTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by ucuc on 2014/03/01.
+//  Copyright (c) 2014年 ucuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ githubTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by ucuc on 2014/03/01.
+//  Copyright (c) 2014年 ucuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
